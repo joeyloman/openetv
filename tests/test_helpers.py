8593748fdb54a5008230be59550b812a332011cc
Yoram Hekma <hekma.yoram@gmail.com>
2016-03-21 10:42:53 +0100

    add open_file tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,4 +1,5 @@
 from openetv_libs import vlc
+from openetv_libs import helpers
 from nose import with_setup
 import tempfile
 import logging
@@ -21,6 +22,42 @@ def delete_pid():
         pass
 
 
+def test_open_file_read_fail():
+    try:
+        helpers.open_file('/nonexistant', 'r', 'error message')
+    except IOError as err:
+        assert err.extra_info == 'error message'
+
+
+def test_open_file_write_fail():
+    try:
+        helpers.open_file('/nonexistant', 'w', 'error message')
+    except IOError as err:
+        assert err.extra_info == 'error message'
+
+
+def test_open_file_write_succeed():
+    fname = tempfile.mkstemp()[1]
+    try:
+        with helpers.open_file(fname, 'w', 'error message') as f:
+            f.write('test')
+        with open(fname, 'r') as f:
+            assert f.read() == 'test'
+    finally:
+        os.remove(fname)
+
+
+def test_open_file_read_succeed():
+    fname = tempfile.mkstemp()[1]
+    with open(fname, 'w') as f:
+        f.write('test')
+    try:
+        with helpers.open_file(fname, 'r', 'error message') as f:
+            assert f.read() == 'test'
+    finally:
+        os.remove(fname)
+
+
 @with_setup(setup=setup_pid, teardown=delete_pid)
 def test_get_pid():
     pid = vlc.get_vlc_pid(pidfile, logging)
@@ -41,3 +78,5 @@ def test_write_pid():
             assert f.read() == '15'
     finally:
         os.remove(pfile)
+
+
